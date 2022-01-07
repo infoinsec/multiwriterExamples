ebbdc473806ae9254ab0b757f7b1511eaf6d7de4
Alex Balliet <alexballiet+infoinsec@gmail.com>
2022-01-06 22:11:32 -0500

    added client key

--- 1/client.js
+++ 1/client.js
@@ -2,7 +2,9 @@ import DHT from '@hyperswarm/dht'
 
 const node = new DHT()
 
-const remotePublicKey = Buffer.from('hex-from-above', 'hex')
+let key = '61549339af45e2aba02c26ff43768285c3d361b5401d3e0cdd2b3598d0940d4c'
+
+const remotePublicKey = Buffer.from(key, 'hex')
 const encryptedSocket = node.connect(remotePublicKey)
 
 encryptedSocket.on('open', function () {
